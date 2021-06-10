5f54da934be8b9d5ae6ac467e831ceec8704913f
aarnavg17 <67118298+aarnavg17@users.noreply.github.com>
2021-06-11 02:36:29 +0530

    HackerRank

---
+++ HackerRank/Problem Solving/Algorithms/Bit Manipulation/Lonely Integer/main.py
@@ -0,0 +1,6 @@
+_ = input()
+a = list(map(int, input().split()))
+for x in a:
+    if a.count(x) == 1:
+        print(x)
+        break
---
+++ HackerRank/Problem Solving/Algorithms/Strings/Pangrams/main.py
@@ -0,0 +1,2 @@
+print("pangram" if len(set(input().lower())) == 27 else "not pangram")
+
